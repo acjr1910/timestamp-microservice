54407af27ab406c8db5453487cea0971d95b56d7
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2020-12-02 10:27:44 -0300

    chore: add fcc files

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const timestamp = require("./routes/timestamp");
 
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
+
 app.use(express.static("public"));
 app.use("/api/timestamp", timestamp);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
 const port = process.env.PORT || 8000;
